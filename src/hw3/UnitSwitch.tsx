6b8c0ef31c9ba1b3612a45d2765e36bc93fd482a
davidluzhiyun <david.luzhiyun@gmail.com>
2024-09-19 23:50:54 -0400

    Unit Switch Button formatted

--- src/hw3/UnitSwitch.tsx
+++ src/hw3/UnitSwitch.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { WeatherData } from "./Weather";
+import "./unitswitch.css";
 
 // Define the props interface
 interface UnitSwitchProps {
@@ -27,6 +28,8 @@ export default function UnitSwitch({ weatherData, setWeatherData }: UnitSwitchPr
     function onSwitchUnit(){
         setWeatherData({ ...weatherData, metricSystemFlag:!(weatherData.metricSystemFlag)});
     }
-    return(<button onClick={onSwitchUnit}>{unitText}</button>)
+    return(<div className="container">
+            <button onClick={onSwitchUnit} className="unit-switch-button">{unitText}</button>
+            </div>)
 }
         
