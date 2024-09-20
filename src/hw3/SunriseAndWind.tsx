9f79d83d4b99b04905df0f0ad4a7bb3b96130f68
davidluzhiyun <david.luzhiyun@gmail.com>
2024-09-20 02:24:19 -0400

    WindandSunset State complete

--- src/hw3/SunriseAndWind.tsx
+++ src/hw3/SunriseAndWind.tsx
@@ -9,7 +9,27 @@ interface SunriseAndWindProp {
 
 export default function SunriseAndWind({ weatherData }: SunriseAndWindProp) {
     const[sunrise, setSunrise] = useState<string>("-");
-    const[sunrset, setSunset] = useState<string>("-");
+    const[sunset, setSunset] = useState<string>("-");
     const[windSpeed, setWindSpeed] = useState<string>("-");
-    const[windDirection, setSunrize] = useState<string>("-");
+    const[windDirection, setWindDirection] = useState<string>("-");
+    useEffect(()=>{
+        if (weatherData.validZip===""){
+            setSunrise("-");
+            setSunset("-");
+            setWindSpeed("-");
+            setWindDirection("-");
+        }
+        else{
+            setSunrise(weatherData?.payload?.forcast?.forcastday[0]?.astro?.surise);
+            setSunset(weatherData?.payload?.forcast?.forcastday[0]?.astro?.sunset);
+            setWindDirection(weatherData?.payload?.current?.wind_dir);
+            if(weatherData.metricSystemFlag){
+                setWindSpeed(weatherData?.payload?.current?.wind_kph);
+            }
+            else{
+                setWindSpeed(weatherData?.payload?.current?.wind_mph)
+            }
+        }
+    }
+    ,[weatherData]);    
 }
